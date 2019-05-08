de4ad570f0998287fe1ffffa72c819735d16121b
wah0713 <wah0713@126.com>
2019-05-08 14:24:10 +0800

    代码优化

--- douyuRemoveAds.user.js
+++ douyuRemoveAds.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         斗鱼去火箭横幅(贵族弹幕样式&&聊天区域铭牌)
 // @namespace    https://github.com/wah0713/myTampermonkey
-// @version      1.7
-// @description  增加 聊天框简化、背景图是否显示、竞猜是否显示、弹幕悬停关闭、登录开启最高画质 （功能按钮）。去除 贵族弹幕样式&&聊天区域铭牌、火力全开（输入框上方）、播放器内关注按钮、右侧浮动广告、底部广告、抽奖中间部提示框、火箭横幅、亲密互动(播放器左下角)、抽奖(播放器左下角)、贵族入场提醒（输入框上方）、页游签到奖励（播放器左下角）、分享 客户端 手游中心（播放器右上角）、导航栏客户端按钮、播放器内主播推荐关注弹幕、播放器内房间号日期（播放器内左下角）、播放器左侧亲密互动、播放器左下角下载客户端QR、未登录提示、分区推荐弹幕
+// @version      1.71
+// @description  一个兴趣使然的脚本，本来只是屏蔽火箭横幅的脚本，到后来。。。
 // @supportURL   https://github.com/wah0713/myTampermonkey/issues
 // @author       wah0713
 // @compatible   chrome
@@ -16,15 +16,6 @@
 
 (function () {
     if (!/^\/\d+$/.test(window.location.pathname) && window.location.pathname.indexOf('topic') === -1) return false
-
-    // // styleTest
-    // const node = document.createTextNode(`
-    // .damuItem-31f924 .text2-4ec68c {
-    //     position: absolute !important;
-    //     background: red !important;
-    //   }
-    // `)
-    // $('head').append($(`<style type="text/css"></style>`).append(node))
 
     const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
     let sign = 0
@@ -83,36 +74,51 @@
 
     // 右侧自定义按钮模块
     $('body').append('<div id="wah0713"></div>')
+
+    // 用户默认配置
+    let defaultConfig = {
+        adjustClarity: false, // 登陆最高画质
+        danmuMove: false, // 弹幕悬停
+        guessIsShow: false, // 竞猜显示
+        backgroundIsShow: false, // 背景显示
+        chatBoxCleaning: true, // 聊天框简化
+        forbiddenMessage: false, // 禁言消息显示
+    }
+    let config = GM_getValue('Config', defaultConfig)
+
+    window.onbeforeunload = () => {
+        GM_setValue('Config', config)
+    }
+
     /**
      *  封装按钮显示事件
      * @param {string} localStorageName 按钮本地存储名
      * @param {string} displayName 按钮显示名
-     * @param {number} index 按钮排序位置
-     * @param {boolean} defaultState 默认状态
      */
-    function btnListFun(localStorageName, displayName, index, defaultState) {
-        defaultState = defaultState || false
-        $("#wah0713").append(`<button>${displayName}(close)</button>`)
+    function btnListFun(localStorageName, displayName) {
+        $("#wah0713").append(`<button class='${localStorageName}'>${displayName}(close)</button>`)
 
-        function btnShow() {
-            if (!GM_getValue(localStorageName, defaultState)) {
-                $(`#wah0713 >button:nth-child(${index})`).addClass('close').text(`${displayName}(close)`)
+        function btnInit() {
+            if (!config[localStorageName]) {
+                $(`#wah0713 .${localStorageName}`).addClass('close').text(`${displayName}(close)`)
             } else {
-                $(`#wah0713 >button:nth-child(${index})`).removeClass('close').text(`${displayName}(open)`)
+                $(`#wah0713 .${localStorageName}`).removeClass('close').text(`${displayName}(open)`)
             }
         }
-        btnShow()
-        $(`#wah0713 >button:nth-child(${index})`).click(() => {
-            GM_setValue(localStorageName, !GM_getValue(localStorageName, defaultState))
-            btnShow()
+        btnInit()
+        $(`#wah0713 .${localStorageName}`).click(() => {
+            config[localStorageName] = !config[localStorageName]
+            btnInit()
         })
     }
-    btnListFun('adjustClarity', '登陆最高画质', 1)
-    btnListFun('danmuMove', '弹幕不悬停', 2)
-    btnListFun('guessIsShow', '竞猜不开启', 3, true)
-    btnListFun('backgroundIsShow', '背景不显示', 4, true)
-    btnListFun('chatBoxCleaning', '聊天框简化', 5, true)
-    btnListFun('forbiddenMessage', '禁言消息显示', 6)
+
+    // 按钮事件
+    btnListFun('adjustClarity', '登陆最高画质')
+    btnListFun('danmuMove', '弹幕悬停')
+    btnListFun('guessIsShow', '竞猜显示')
+    btnListFun('backgroundIsShow', '背景显示')
+    btnListFun('chatBoxCleaning', '聊天框简化')
+    btnListFun('forbiddenMessage', '禁言消息显示')
 
     // 左侧展开默认收起
     if ($(".Aside-main--shrink").width() > 100) {
@@ -120,19 +126,19 @@
     }
 
     const observer = new MutationObserver(function () {
-        // remove模块
-        tempArr = removeDomList.slice(0)
-        let i = 0
-        removeDomList.length && removeDomList.forEach((domName, index) => {
-            if ($(domName).remove().length != 0) {
-                tempArr.splice(index + i, 1)
-                i--
-            }
-        })
-        removeDomList = tempArr.slice(0)
+        // // remove模块
+        // tempArr = removeDomList.slice(0)
+        // let i = 0
+        // removeDomList.length && removeDomList.forEach((domName, index) => {
+        //     if ($(domName).remove().length != 0) {
+        //         tempArr.splice(index + i, 1)
+        //         i--
+        //     }
+        // })
+        // removeDomList = tempArr.slice(0)
 
         // 主播公告、贡献周榜、贵宾和粉丝团
-        if (GM_getValue('chatBoxCleaning', true)) {
+        if (config.chatBoxCleaning) {
             $(".layout-Player-asideMainTop").addClass("hide")
         } else {
             $(".layout-Player-asideMainTop").removeClass("hide")
@@ -149,7 +155,7 @@
         }
 
         // 登录开启最高画质
-        if (GM_getValue('adjustClarity', false)) {
+        if (config.adjustClarity) {
             if (notProcessedAdjustClarity && $('.tip-e3420a ul') && $('.tip-e3420a ul').children().length && !$('.tip-e3420a ul li:first-child').hasClass('selected-3a8039')) {
                 $('.tip-e3420a ul li:first-child').click()
                 notProcessedAdjustClarity = false
@@ -157,7 +163,9 @@
         }
 
         // 弹幕悬停关闭
-        if (GM_getValue('danmuMove', false)) {
+        if (config.danmuMove) {
+            $(".danmuItem-31f924 .mask").remove()
+        } else {
             $('.danmuItem-31f924').each((index, dom) => {
                 if (!$(dom)[0].handle) {
                     $(dom)[0].handle = true
@@ -167,7 +175,7 @@
         }
 
         // 是否开启竞猜关闭
-        if (GM_getValue('guessIsShow', true)) {
+        if (config.guessIsShow) {
             $('.guessGameContainer').show()
         } else {
             $('.guessGameContainer').hide()
@@ -180,26 +188,38 @@
         }
 
         // 背景图
-        if (GM_getValue('backgroundIsShow', true)) {
-            // 底部广告、
+        if (config.backgroundIsShow) {
+            $('html').removeClass('no-background')
+            $('#js-bottom').show()
+            $('body')[0].style = ""
+            if ($('.layout-Container')[0]) {
+                $('.layout-Container')[0].style = ""
+            }
+            // 支持url带 /topic
+            if (window.location.pathname.indexOf('topic') > -1) {
+                $('.layout-Main')[0].removeAttribute('style')
+            }
+            $('.bc-wrapper').show()
+        } else {
+            // 底部广告
             $('#js-bottom').hide()
             $('html').addClass('no-background')
             if ($('.layout-Main').offset().top < $(window).height() * 1 / 2) {
                 $('body').css({
                     'background-image': 'none',
-                    'background-color': '#abc'
+                    'background-color': '#ffe'
                 })
             } else {
                 $('body').css({
                     'background-image': "url('https://wah0713.github.io/myTampermonkey/image/down.jpg')",
-                    'background-color': '#f6f6f6',
+                    'background-color': '#ffe',
                     'background-position': 'center 68px',
                     'background-repeat': 'repeat-y'
                 })
             }
             $('.layout-Container') && $('.layout-Container').css({
                 'background-image': 'none',
-                'background-color': '#abc'
+                'background-color': '#ffe'
             })
 
             // 支持url带 /topic
@@ -219,18 +239,6 @@
                 })
             })
             $('.bc-wrapper').not($('.bc-wrapper')[sign]).hide()
-        } else {
-            $('html').removeClass('no-background')
-            $('#js-bottom').show()
-            $('body')[0].style = ""
-            if ($('.layout-Container')[0]) {
-                $('.layout-Container')[0].style = ""
-            }
-            // 支持url带 /topic
-            if (window.location.pathname.indexOf('topic') > -1) {
-                $('.layout-Main')[0].removeAttribute('style')
-            }
-            $('.bc-wrapper').show()
         }
 
         // 火力全开弹幕
@@ -252,7 +260,7 @@
         // 播放器内火力全开获奖
         $('.FirePowerRewardModal').remove()
 
-        // 输入框上方送礼400毫米淡出
+        // 输入框上方送礼3000毫米淡出
         $('#js-player-barrage .BarrageBanner').children().delay(1000 * 3).fadeOut('slow')
 
         // 聊天框用户进入欢迎语
@@ -265,12 +273,10 @@
         $('.Barrage-list .Barrage-guess').parent('.Barrage-listItem').hide()
 
         // 聊天框用户相关消息广播
-        // $('.Barrage-list .Barrage-icon--sys').parent('.Barrage-listItem').hide()
-        // 系统提示（例如禁言）
-        // .Barrage-notice--red
+        // 系统提示（例如禁言）Barrage-notice--red
         $('.Barrage-list .Barrage-icon--sys').each((idx, dom) => {
             const domParent = $(dom).parent('.Barrage-listItem')
-            if (GM_getValue('forbiddenMessage', false)) {
+            if (config.forbiddenMessage) {
                 if (domParent.find('.Barrage-text').text().indexOf("禁言") === -1) {
                     domParent.hide()
                 }
@@ -281,9 +287,18 @@
         // 聊天框用户铭牌
         $('.Barrage-list .Barrage-nickName').prevAll().hide()
     })
-    const config = {
+    const observerConfig = {
         subtree: true,
         childList: true,
     }
-    observer.observe(target, config)
+    observer.observe(target, observerConfig)
+
+    // // styleTest
+    // const node = document.createTextNode(`
+    // html .is-fullScreenPage #wah0713 {
+    //     display: none;
+    //   }
+    // `)
+    // $('head').append($(`<style type="text/css"></style>`).append(node))
+
 })()
