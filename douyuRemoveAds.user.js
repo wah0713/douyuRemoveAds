135f1beae18bb6a79dda35db077aa338c6d9179b
wah <wah0713@126.com>
2019-08-01 23:48:42 +0800

    彩蛋

--- douyuRemoveAds.user.js
+++ douyuRemoveAds.user.js
@@ -9,6 +9,7 @@
 // @license      MIT
 // @icon         https://www.douyu.com/favicon.ico
 // @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// @require      https://greasyfork.org/scripts/388079-canvas%E5%8A%A8%E7%94%BB/code/canvas%E5%8A%A8%E7%94%BB.js?version=721543
 // @match        http*://www.douyu.com/*
 // @run-at       document-idle
 // @grant        GM_setValue
@@ -218,6 +219,12 @@
                     // 提示用户更新内容
                     GM_setValue(version, true)
                     $('#wah0713-alert').show()
+                    // 彩蛋
+                    if(!config.backgroundIsShow){
+                        $('#easterEgg').show()
+                    } else {
+                        $('#easterEgg').hide()
+                    }
                 })
             }
         }
@@ -516,6 +523,8 @@
                 OriginalLayoutMainParentMarginTop && $layoutMainParent.css('margin-top', OriginalLayoutMainParentMarginTop)
 
                 $('body').addClass('backgroundIsShow')
+                // 彩蛋
+                $('#easterEgg').hide()
             } else {
                 // 播放器位置
                 $('.Background-holder').css('padding-top', 10)
